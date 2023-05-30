75d2565079525a3d3238e83027c66512150d93c7
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-30 18:21:58 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('.main_slide').slick({
+        arrows: false,
+        // vertical 밑에서 위로 올라가는거 
+        // fade 깜박거리면서 사라지는거
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev');
+    });
+
+
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext');
+    });
+
+    $('.main_visual .controll .play').on('click', function () {
+        $('.main_slide').slick('play');
+    });
+    $('.main_visual .controll .pause').on('click', function () {
+        $('.main_slide').slick('pause');
+    });
+
+
+});
